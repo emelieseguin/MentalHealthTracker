e52d558cac7858e4ba7f25be4e12643a5d4bccea
emelieseguin <emelieseguin@msn.com>
2019-06-22 15:02:27 -0400

    Added personInfo class and updated the profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,17 +1,41 @@
 import { Component, OnInit } from "@angular/core";
 import { isAndroid, Page } from "tns-core-modules/ui/page/page";
 
+class PersonInfo {
+    firstName: string;
+    lastName: string;
+    age?: number;
+    sex?: string;
+    email?: string;
+    diagnoses?: string[];
+    treatments?: string[];
+}
+
+const FakePerson: PersonInfo = {
+    firstName: 'John',
+    lastName: 'Smith',
+    diagnoses: [
+        'whatever',
+        'stuff'
+    ]
+}
+
 @Component({
     selector: "Profile",
     moduleId: module.id,
     templateUrl: "./profile.component.html"
 })
 export class ProfileComponent implements OnInit {
-    
+    currentUser: PersonInfo;
+
+
     constructor(private page: Page) {
         if (isAndroid) {
             this.page.actionBarHidden = true;
         }
+
+        this.currentUser = FakePerson;
+        //console.log(person.firstName); 
     }
 
     ngOnInit(): void { 
